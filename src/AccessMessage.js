961d578a41012fe1cab931c5918f7dd27be84c5b
alex <alexzelinsky124@gmail.com>
2022-09-14 20:44:57 -0700

    new-source-code text file created

--- src/AccessMessage.js
+++ src/AccessMessage.js
@@ -4,6 +4,7 @@ function AccessMessage({ isHacked }) {
   return (
     <div id="access-msg" className={isHacked ? "success" : "denied"}>
       {isHacked ? "Access Granted" : "Access Denied"}
+      <span className="msg-sub-head">(Press Esc)</span>
     </div>
   );
 }
